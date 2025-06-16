98e8d35d001542dbe4650e549a7116eb22586943
ruben <216598016+galeaseli@users.noreply.github.com>
2025-06-16 23:18:01 +0100

    Step Four Completed

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -6,5 +6,13 @@ using UnityEngine.UI;
 public class LogicScript : MonoBehaviour
 {
     public int playerScore;
+    public Text scoreText;
 
+    [ContextMenu("Increase Score")]
+    public void addScore()
+    {
+        playerScore = playerScore + 1;
+        scoreText.text = playerScore.ToString();
+    }
+    
 }
